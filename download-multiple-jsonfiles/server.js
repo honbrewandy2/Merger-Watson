adcde1e8767f18ec282984cc74c4da6641fdf2ac
GodsonAddy <70777404+GodsonAddy@users.noreply.github.com>
2021-08-06 12:43:20 +0000

    merge files

---
+++ download-multiple-jsonfiles/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const cors = require('cors')
+const jsonConcat = require('json-concat')
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cors())
+
+jsonConcat({
+    src: './public/uploads',
+    dest: './config.js'
+}, function(json) {
+    console.log(json)
+})
+
+app.post('/', (req,res) => {
+    try {
+        const files = [];
+        const dir = __filename;
+        fs.readFileSync(dir).forEach((file) => {
+            files.push(file)
+        })
+        res.download(req.file)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+const port = 5000;
+
+app.listen(port, () => {
+    console.log("Server is listening to port", port)
+})
---
+++ download-multiple-jsonfiles/src/components/HelloWorld.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="hello">
+    <div>
+      <b-alert
+        variant="success"
+        dismissible
+        fade
+        v-model="showSuccessAlert"
+      >
+        Merging of files was a success!
+      </b-alert>  
+      <b-alert
+        variant="danger"
+        dismissible
+        fade
+        v-model="showErrorAlert"
+      >
+        Merging of files was a failure! Please try again
+      </b-alert>
+    </div>
+
+    <div>
+      <b-form @submit.prevent="mergeFiles" v-model="jsonfile">
+        <b-form-file multiple enctype="multipart/form-data">
+          <template slot="file-name" slot-scope="{ names }" accept="file/*">
+            <b-badge variant="dark">{{ names[0] }}</b-badge>
+            <b-badge v-if="names.length > 1" variant="dark" class="ml-1">
+              + {{ names.length - 1 }} More files
+            </b-badge>
+          </template>
+        </b-form-file>
+
+        <div class="spinner">
+          <b-button type="submit" variant="success" v-if="loading" block>Merge files</b-button>
+          <b-button type="submit" variant="success" v-if="!loading" disabled>
+            <b-spinner small type="grow"></b-spinner>
+            Loading...
+          </b-button>
+        </div>
+      </b-form>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: "HelloWorld",
+  data () {
+    return {
+      loading: false,
+      jsonfile: [],
+      showSuccessAlert: false,
+      shoeErrorAlert: false
+    }
+  },
+  methods: {
+    async mergeFiles () {
+      const payload = new FormData()
+      for (const file of this.jsonfile) {
+        payload.append('jsonfile', file, file.name)
+      }
+      this.loading = true
+      try {
+        await axios.post('http://localhost:5000', payload)
+        this.loading = false
+        this.jsonfile = []
+        this.showSuccessAlert = true
+        this.shoeErrorAlert = false
+      } catch (error) {
+        this.loading = false
+        this.jsonfile = []
+        this.showSuccessAlert = true
+        this.shoeErrorAlert = false
+        console.log(error)
+      }
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 100px;
+}
+.spinner{
+  margin-top: 30px;
+}
+</style>
